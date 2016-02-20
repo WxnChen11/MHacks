121d2814ec95461aeb4025933c3e76abf475232a
Wenxin Chen <wenxinchen11@hotmail.com>
2016-02-20 11:57:16 -0500

    initial commit

---
+++ WiiUPCController/src/com/johnfreed/wiiupccontroller/Main.java
@@ -0,0 +1,22 @@
+package com.johnfreed.wiiupccontroller;
+
+public class Main {
+
+ public static void main(String[] args) {
+  try {
+   // Set up the controller
+   //PCController controller = new PCController();
+   
+   // Set up the server
+   WiiUServer server = new WiiUServer();
+   server.setPort(1337);
+   server.setPrintDebugStatements(true);
+   
+   // Start the server
+   server.StartServer();
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+ }
+
+}
